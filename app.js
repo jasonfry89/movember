803f6cc520685f741a3f43ca7250047d771e9f02
Jason Fry <jasonfry89@gmail.com>
2013-10-29 18:06:32 -0500

    port fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 app.use(express.favicon());
